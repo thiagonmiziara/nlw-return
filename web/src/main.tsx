f03aa413c3ae6bcbe157c96ddb603fc8a03c133f
thiagonmiziara <thiagonmiziara@gmail.com>
2022-05-08 19:35:33 -0300

    ajuste snackbar

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { SnackbarProvider } from "notistack";
+import App from "./App";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SnackbarProvider>
+    <SnackbarProvider maxSnack={3} preventDuplicate>
       <App />
     </SnackbarProvider>
   </React.StrictMode>
